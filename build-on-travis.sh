46503479c527e88a5c5217352d7a4a65d14ce6cd
Mark Eschbach <meschbach@gmail.com>
2018-04-15 23:04:19 -0700

    Quietl ActiveMQ lib extraction

--- build-on-travis.sh
+++ build-on-travis.sh
@@ -4,7 +4,7 @@
 # Install Apache CMS
 #
 curl http://mirror.cogentco.com/pub/apache/activemq/activemq-cpp/3.9.4/activemq-cpp-library-3.9.4-src.tar.bz2 -o activemq-cpp-library.tar.bz2
-tar xvf activemq-cpp-library.tar.bz2
+tar xf activemq-cpp-library.tar.bz2
 cd activemq-cpp-library*
 ./configure --prefix=$PWD/../amq-cms
 make LDFLAGS="-pthread `./amq-cms/bin/activemqcpp-config --libs`" CXXFLAGS="`./amq-cms/bin/activemqcpp-config --cflags` -std=c++17 -Wno-deprecated-declarations -pthread"
